97de23fb71ac9d98e7bdb5ae0dedd8203c749b0f
Chris Bailey <chris@ghostly.io>
2020-12-09 23:35:21 -0500

    feat: write frontmatter to new mdx file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use chrono::prelude::*;
 use dialoguer::{theme::ColorfulTheme, Input};
+use std::fs::File;
+use std::io::prelude::*;
 use str_slug::slug;
 
 fn main() {
+    // user input for post title and excerpt
     let title: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Post title")
         .interact_text()
         .unwrap();
-
-    let slug: String = slug(&title);
 
     let excerpt: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Post excerpt")
@@ -17,10 +18,20 @@ fn main() {
 
     let date: DateTime<Local> = Local::now();
 
+    // generate the file name, and post slug
+    let slug: String = slug(&title);
+    let file_slug: String = format!("{}-{}.mdx", date.format("%Y-%m-%d"), slug);
+
     let frontmatter = format!(
         "---\ntitle: {}\nexcerpt: {}\ndate: {}\nslug: {}\n---",
         &title, &excerpt, &date, &slug
     );
 
-    println!("{}", frontmatter);
+    create_mdx_file(&file_slug, &frontmatter).unwrap();
+}
+
+fn create_mdx_file(slug: &String, frontmatter: &String) -> std::io::Result<()> {
+    let mut file = File::create(slug)?;
+    file.write_all(frontmatter.as_bytes())?;
+    Ok(())
 }
