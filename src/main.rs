94c6428cb3a21b4f1d1d70065a46e978f5bae7f9
Chris Bailey <chris@ghostly.io>
2020-12-10 23:55:17 -0500

    feat: arg parsing with structopt for file path

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ use chrono::prelude::*;
 use dialoguer::{theme::ColorfulTheme, Input};
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::PathBuf;
 use str_slug::slug;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    #[structopt(short, long, parse(from_os_str))]
+    path: PathBuf,
+}
 
 fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt.path);
     // user input for post title and excerpt
     let title: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Post title")
@@ -30,7 +41,7 @@ fn main() {
     create_mdx_file(&file_slug, &frontmatter).unwrap();
 }
 
-fn create_mdx_file(slug: &String, frontmatter: &String) -> std::io::Result<()> {
+fn create_mdx_file(slug: &str, frontmatter: &str) -> std::io::Result<()> {
     let mut file = File::create(slug)?;
     file.write_all(frontmatter.as_bytes())?;
     Ok(())
