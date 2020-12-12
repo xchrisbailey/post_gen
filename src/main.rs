58eb32da6b922adfb3096dcf5eb3235cbdf04e87
Chris Bailey <chris@ghostly.io>
2020-12-11 23:29:49 -0500

    feat: option for markdown or mdx extension

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::prelude::*;
-use dialoguer::{theme::ColorfulTheme, Input};
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 use std::fs::File;
 use std::io::prelude::*;
 use str_slug::slug;
@@ -26,24 +26,32 @@ fn main() {
         .interact_text()
         .unwrap();
 
+    let extensions: Vec<&str> = vec!["mdx", "md"];
+    let ext: usize = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Markdown or MDX")
+        .default(0)
+        .items(&extensions[..])
+        .interact()
+        .unwrap();
+
     let date: DateTime<Local> = Local::now();
 
     // generate the file name, and post slug
     let slug: String = slug(&title);
-    let file_slug: String = format!("/{}-{}.mdx", date.format("%Y-%m-%d"), slug);
+    let file_slug: String = format!("/{}-{}", date.format("%Y-%m-%d"), slug);
 
     let frontmatter = format!(
         "---\ntitle: {}\nexcerpt: {}\ndate: {}\nslug: {}\n---",
         &title, &excerpt, &date, &slug
     );
 
-    let save_path: String = format!("{}/{}", opt.path, file_slug);
+    let save_path: String = format!("{}{}.{}", opt.path, file_slug, extensions[ext]);
 
     create_mdx_file(&frontmatter, &save_path).unwrap();
 }
 
 fn create_mdx_file(frontmatter: &str, save_path: &str) -> std::io::Result<()> {
-    let mut file = File::create(save_path)?;
+    let mut file: File = File::create(save_path)?;
     file.write_all(frontmatter.as_bytes())?;
     Ok(())
 }
