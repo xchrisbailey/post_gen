4ae90918b3c03602e296f7a34e2b282965551b44
Chris Bailey <chris@ghostly.io>
2020-12-11 22:05:43 -0500

    feat: save mdx file to specified path

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
 struct Opt {
-    #[structopt(short, long, parse(from_os_str))]
-    path: PathBuf,
+    #[structopt(short, long)]
+    path: String,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt.path);
+
     // user input for post title and excerpt
     let title: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Post title")
@@ -31,18 +31,20 @@ fn main() {
 
     // generate the file name, and post slug
     let slug: String = slug(&title);
-    let file_slug: String = format!("{}-{}.mdx", date.format("%Y-%m-%d"), slug);
+    let file_slug: String = format!("/{}-{}.mdx", date.format("%Y-%m-%d"), slug);
 
     let frontmatter = format!(
         "---\ntitle: {}\nexcerpt: {}\ndate: {}\nslug: {}\n---",
         &title, &excerpt, &date, &slug
     );
 
-    create_mdx_file(&file_slug, &frontmatter).unwrap();
+    let save_path: String = format!("{}/{}", opt.path, file_slug);
+
+    create_mdx_file(&frontmatter, &save_path).unwrap();
 }
 
-fn create_mdx_file(slug: &str, frontmatter: &str) -> std::io::Result<()> {
-    let mut file = File::create(slug)?;
+fn create_mdx_file(frontmatter: &str, save_path: &str) -> std::io::Result<()> {
+    let mut file = File::create(save_path)?;
     file.write_all(frontmatter.as_bytes())?;
     Ok(())
 }
